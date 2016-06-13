3b2b9f5330bcdb2c7ba0f23ef7503218ac4682d8
arnob-mtk <arnobislam0013@gmail.com>
2016-06-13 13:16:16 +0600

    Add files via upload

---
+++ RX2-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/RX2/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/RX2/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/RX2/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/RX2/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/WALTON/RX2/proprietary/vendor,system/vendor)
